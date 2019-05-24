67108cdb45c45e505d83b1784e794a75542cc95f
Matheus Nobrega <39496767+MatheusNobrega@users.noreply.github.com>
2019-05-24 12:17:40 -0300

    Adicionando function Contraste

---
+++ js/funcao.js
@@ -0,0 +1,6 @@
+function contraste(){$('body, header, nav, main, footer, button,a,h1,h2,h3,h4,h5,label,input,textarea, select,span').addClass("coresImportantes")
+}
+function risco(){$('footer').addClass("risco")
+}
+function bot(){$('button').addClass("bot")
+}
