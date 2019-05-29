3d7d161a1cd12070b82f262f99a863d5281d8301
daviguelfi <daviguelfi@gmail.com>
2019-05-29 17:52:09 -0300

    Code do captcha

--- js/funcao.js
+++ js/funcao.js
@@ -12,3 +12,58 @@ function bot() {
   $('button').
   addClass("bot")
 }
+
+
+//reference https://codepen.io/nicolas-justin/pen/jXQjQr?editors=1000
+// Toogle Click Math
+var toggle = document.getElementById('toggle');
+var toggleIcon = document.getElementById('toggleIcon');
+
+// Math Captcha
+var math = document.getElementById('math');
+var calc = document.getElementById('calc');
+var result = 0;
+// Numbers
+toggle.addEventListener('click', toggleFunction);
+
+function toggleFunction() {
+  if (toggle.value == 1) {
+    // Numbers
+    v1 = Math.floor(Math.random() * 10);
+    v2 = Math.floor(Math.random() * 10);
+    result = v1 + v2;
+    math.innerHTML = v1 + ' + ' + v2;
+    toggleIcon.setAttribute('class', 'fas fa-toggle-on toggle-off');
+    toggle.style.opacity = ('.3');
+    toggle.value = 0;
+  } else {
+    // Numbers
+    var v1 = Math.floor(Math.random() * 10);
+    var v2 = Math.floor(Math.random() * 10);
+    result = v1 * v2;
+    math.innerHTML = v1 + ' * ' + v2;
+    toggleIcon.setAttribute('class', 'fas fa-toggle-on toggle-on');
+    toggle.style.opacity = ('1');
+    toggle.value = 1;
+  }
+}
+
+function calcMath() {
+  if (calc.value == result) {
+    btnSI.removeAttribute('disabled');
+    calc.style.borderColor = "#ccc";
+  } else {
+    btnSI.setAttribute('disabled', 'disabled');
+    calc.style.borderColor = "#f00";
+  }
+}
+// Disabled button sign in
+var btnSI = document.getElementById('btnSignIn');
+
+function btnDisabled() {
+  if (calc.value == '') {
+    btnSI.setAttribute('disabled', 'disabled');
+  } else {
+    btnSI.setAttribute('disabled', 'none');
+  }
+}
